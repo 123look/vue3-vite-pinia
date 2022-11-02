2c3a33ad0b0f81589204db2d45fad2d96c1f28d4
luojie <yyluojie@qq.com>
2022-11-02 17:44:23 +0800

    last

--- src/pages/system/dispose/index.vue
+++ src/pages/system/dispose/index.vue
@@ -1,8 +1,12 @@
 <template>
-    配置
+    <quark-button type="primary">默认按钮</quark-button>
+    <quark-marquee :title="title" speed="200"></quark-marquee>
 </template>
 <script setup lang='ts'>
 import { ref, reactive } from 'vue'
+import 'quarkd/lib/button';
+import "quarkd/lib/marquee";
+const title = ref('1231231231231231231231232123123132132122312321')
 </script>
 <style scoped>
 </style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -25,7 +25,6 @@ export const userInfoStore = defineStore(Names.Test, {
                 cate_name: '',
                 cate_alias: ''
             }
-            
         }
     },
     getters: {
